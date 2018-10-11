c39885b16cd99423169bf93db9a7af5826b64d8d
Avadesh ji <avadesh.kumar@go-mac-avadeshk.local>
2018-10-11 10:55:10 +0530

    Initial Commit

---
+++ S3FileUploadSignv4iOS/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  S3FileUploadSignv4iOS
+//
+//  Created by Avadesh Kumar on 11/10/18.
+//  Copyright © 2018 Avadesh Kumar. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ S3FileUploadSignv4iOSTests/S3FileUploadSignv4iOSTests.swift
@@ -0,0 +1,36 @@
+//
+//  S3FileUploadSignv4iOSTests.swift
+//  S3FileUploadSignv4iOSTests
+//
+//  Created by Avadesh Kumar on 11/10/18.
+//  Copyright © 2018 Avadesh Kumar. All rights reserved.
+//
+
+import XCTest
+@testable import S3FileUploadSignv4iOS
+
+class S3FileUploadSignv4iOSTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
